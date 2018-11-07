6440eea3785035a4e6bb2faed63f66f39dae2981
Carter Johnson <caljohnson@ucdavis.edu>
2018-11-07 10:45:55 -0800

    ran some comparative PRC and H-function figures

--- PRCcode/compare_PRCs.m
+++ PRCcode/compare_PRCs.m
@@ -1,12 +1,12 @@
-clear
-global t_f t_n t_m c I dt
+global t_f t_n t_m c I dt a
 
-tees_m = 1:10;
+cees = 2:0.125:3;
 
-for ll = 1:size(tees_m,2)
+for ll = 1:size(cees,2)
 % -- NM MODEL PARAMETERS --
-t_f=5; t_n = 0.1; t_m = tees_m(ll); %timescales for length, neural, and muscule activity
-c = 1; I = 0; %total feedback strength, AVB input bias
+t_f=5; t_n = 0.1; t_m = 1; %timescales for length, neural, and muscule activity
+c = cees(ll); I = 0; %total feedback strength, AVB input bias
+a = 1;
 nv=5; % number of variables in model - 2 neurons, 2 muscles, 1 curvature
 
 % -- coupling parameters --
@@ -14,23 +14,21 @@ nv=5; % number of variables in model - 2 neurons, 2 muscles, 1 curvature
 eps_prop = 0.1;
 
 %external viscosity (mechanical coupling)
-gamma = 1;
+gamma = 1e-3;
 
 % time step size
-dt0=0.001;
-
-
+dt0=1e-3;
 
 % ----  I. FIND PERIODIC ORBIT  ----
 
-nmax=100000;  % maximal number of time steps in one period
+nmax=1e8;  % maximal number of time steps in one period
 Kmark=0.1;  % initial and final curvature (K) in periodic orbit (arbitrary!)
 
 dt=dt0;
 x0(1:nv)=[0;1;-1;0;0]; % initial state (K,V1,V2,A1,A2)
 
 dx=10000.0;     
-dxcrit=0.000001 % precision for periodic orbit
+dxcrit=1e-5 % precision for periodic orbit
 
 while (dx>dxcrit) 
 % iterate until periodic orbit found to desired precision (dxcrit).
@@ -62,17 +60,6 @@ v2(1:ii-imin+1,1:nv)=v(imin:ii,1:nv);
 v2(ii-imin+2:ii,1:nv)=v(1:imin-1,1:nv);
 v=v2;
 
-% %display result
-% figure(1); clf;
-%  xlim([0, ii*dt]);
-% subplot(4,1,1); plot(v(:,1),'Linewidth', 4); ylabel('\kappa');
-% subplot(4,1,2); plot(v(:,2), 'g','Linewidth', 4); hold on; plot(v(:,3), 'r','Linewidth', 4); 
-% ylabel('V'); legend('V', 'D');
-% subplot(4,1,3); plot(v(:,4), 'g','Linewidth', 4); hold on; plot(v(:,5), 'r','Linewidth', 4); 
-% ylabel('A'); legend('V', 'D');
-% subplot(4,1,4); plot(tanh(v(:,4)-2)+1, 'g','Linewidth', 4); hold on; plot(tanh(v(:,5)-2)+1, 'r','Linewidth', 4); 
-% ylabel('\sigma(A)'); legend('V', 'D');
-% suptitle('Cycle timetraces');
 % OUTPUT:   v(1:ii,1:nv) contains periodic orbit
 %           (ii-1)*dt=~period of orbit
 
@@ -106,18 +93,81 @@ end;
 sc0=median(sc);
 z=z/sc0;
 
-% %display result
-% figure(2); clf; xlim([0, ii*dt]); suptitle('PRCs');
-% subplot(3,1,1); plot(z(:,1), 'Linewidth', 4); ylabel('\kappa');
-% subplot(3,1,2); plot(z(:,2), 'g', 'Linewidth', 4); hold on; plot(z(:,3), 'r','Linewidth', 4); 
-% ylabel('V'); legend('V', 'D');
-% subplot(3,1,3); plot(z(:,4), 'g','Linewidth', 4); hold on; plot(z(:,5), 'r','Linewidth', 4); 
-% ylabel('A'); legend('V', 'D');
+% %Plot PRC for A and sigma(A)
+% figure(ll); clf;
+% xlim([0, ii*dt]);
+% subplot(2,1,1); plot(z(:,4), 'g','Linewidth', 4); hold on; plot(z(:,5), 'r','Linewidth', 4); 
+% ylabel('A'); legend('V', 'D'); title(['A_V, A_D PRCs, c = ' num2str(c)]);
+% subplot(2,1,2); plot(tanh(v(:,4)-2)+1, 'g','Linewidth', 4); hold on; plot(tanh(v(:,5)-2)+1, 'r','Linewidth', 4); 
+% ylabel('\sigma(A)'); legend('V', 'D'); title('\sigma(A_V), \sigma(A_D) Cycle Timetrace');
+%
 
+%Plot PRCs
+figure(ll); clf; xlim([0, ii*dt]); suptitle(['PRCs, c = ' num2str(c)]);
+subplot(3,1,1); plot(z(:,1), 'Linewidth', 4); ylabel('\kappa');
+subplot(3,1,2); plot(z(:,2), 'g', 'Linewidth', 4); hold on; plot(z(:,3), 'r','Linewidth', 4); 
+ylabel('V'); legend('V', 'D');
+subplot(3,1,3); plot(z(:,4), 'g','Linewidth', 4); hold on; plot(z(:,5), 'r','Linewidth', 4); 
+ylabel('A'); legend('V', 'D');
+saveas(figure(ll), ['compare_prc_plots/PRCs_c_' num2str(c) '.png']);
+
+% OUTPUT:   z(1:ii,1:nv) contains iPRC
+
+% ---- III.  CALCULATE G-FUNCTION  ----
+% ---------- for the pair --------------
+
+% intrinsic limit cycle info
+t=[0:dt:(ii-1)*dt]; 
+p=t(ii); % period
+
+% -- Mechanical coupling ---
+dim = 2; %pair of 2 units
+%second-difference matrix A
+A = spdiags([ones(dim,1) -2*ones(dim,1) ones(dim,1)], [0 1 2], dim, dim+2);
+B = inv(full(A*A'));
+%K' from limit cycle
+Kp = dv;
+Kp(ii+1) = Kp(1);
+%mech coupling terms
+% e = (A*A')\(gamma*Kp);
+
+% calculate H-functions 
+% ---------------------
+for j=1:ii
+    %mechanical coupling
+  m1 = z(j:ii,1).*transpose(Kp(1:ii+1-j));
+  m2 = z(1:j-1,1).*transpose(Kp(ii+2-j:ii));
+    %h function - mech
+  h1m(j)=(sum(m1)+sum(m2))*dt0/p;
+  h2m(ii+1-j)=(sum(m1)+sum(m2))*dt0/p;
+    %proprioceptive coupling
+  p1=z(j:ii,2).*v(1:ii+1-j,1);
+  p2=z(1:j-1,2).*v(ii+2-j:ii,1);
+    %h function - prop
+  h1p(j)=(sum(p1)+sum(p2))*dt0/p;
+  h2p(ii+1-j)=(sum(p1)+sum(p2))*dt0/p;
+end;
+
+% calculate G-functions for the Pair
+% ---------------------
+%mech coupling is symmetric in the pair!
+g1=B(2,1)*(h1m-h2m);
+ming1 = min(abs(g1));
+%prop coupling is not
+% g2 =(h1p-h2p);
+minh1p = min(abs(h1p));
 figure(ll); clf;
-xlim([0, ii*dt]);
-subplot(2,1,1); plot(z(:,4), 'g','Linewidth', 4); hold on; plot(z(:,5), 'r','Linewidth', 4); 
-ylabel('A'); legend('V', 'D'); title(['A_V, A_D PRCs, \tau_m = ' num2str(t_m)]);
-subplot(2,1,2); plot(tanh(v(:,4)-2)+1, 'g','Linewidth', 4); hold on; plot(tanh(v(:,5)-2)+1, 'r','Linewidth', 4); 
-ylabel('\sigma(A)'); legend('V', 'D'); title('\sigma(A_V), \sigma(A_D) Cycle Timetrace');
+plot(t(1:ii),g1,'r', 'linewidth',2); hold on; 
+plot(t(1:ii),h1p,'g', 'linewidth',2);
+plot([0,p],[0,0],'k:','linewidth',2);
+plot(t(abs(g1)<=1e-4),0*t(abs(g1)<=1e-4),'ko','Markersize',10);
+plot(t(abs(h1p)<=1e-4),0*t(abs(h1p)<=1e-4),'ko','Markersize',10);
+legend('mechanical','proprioceptive','"zero"');
+xlabel('time(ms)'); 
+title(['G-functions for NM paired-oscillator model, c = ' num2str(c)]);
+saveas(figure(ll), ['compare_g_fns/g_fns_c' num2str(c) '.png']);
+
+
+
+clear v v2 sc z t h1m h2m h1p h2p
 end
