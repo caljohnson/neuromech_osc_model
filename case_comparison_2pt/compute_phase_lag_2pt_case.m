bf4835ed95c484c0f3a352087210de86012902d2
Carter Johnson <caljohnson@ucdavis.edu>
2018-12-10 15:23:22 -0800

    added simulation loop and new fns

---
+++ case_comparison_2pt/compute_phase_lag_2pt_case.m
@@ -0,0 +1,64 @@
+function [ p_diff ] = compute_phase_lag_2pt_case( Kappa, ploton)
+%compute_phase_lags Computes phase lags between the pair
+
+%  Uses cross-correlation to get phase lags between each oscillator unit
+%  Input: Kappa - gridsz*dim vector of curvatures at each point in body
+%          ploton - default 0 for off, pass 1 to plot
+%
+%  Output: 
+%          p_diff - phase diff of oscillator #2 rel to osc. #1
+%          
+%          
+
+global TF max_step dim
+
+%if no ploton command passed, default to 0
+if nargin < 2
+  ploton = 0;
+end
+
+%get second-half of time series
+Kappa = Kappa(1:end,(TF/max_step)/2:TF/max_step-1);
+%get autocorrelation of first oscillator
+control_corr =  xcorr(Kappa(1,:));
+%find peaks of autocorr
+[vals,locs] = findpeaks(control_corr);
+[~, sorted_locs] = sort(vals,'descend');
+%peak-to-peak distance defines period of oscillation
+period_cycle = abs(locs(sorted_locs(1))-locs(sorted_locs(2)));
+%get max-peak index of autocorr
+[~,I_control] = max(control_corr);
+
+%compute phase differences
+phase_diff = zeros(dim,1);
+winding_no = 0;
+for ii=2:dim
+   %compute cross-correlation between oscillator ii and 1 
+   cross_cor = xcorr(Kappa(ii,:), Kappa(1,:));
+   %get index of max-peak of cross-corr
+   [~,I_new] = max(cross_cor);
+   %phase difference is peak-peak distance / cycle period
+   phase_diff(ii) = (I_new-I_control)/period_cycle;
+   phase_diff(ii) = phase_diff(ii) + winding_no;  %add winding number
+   
+   %shift phase differences so that negative phase differences are phase
+   %advances, keep track of winding number
+   if phase_diff(ii) < phase_diff(ii-1)
+       phase_diff(ii) = phase_diff(ii) + 1;
+       winding_no = winding_no+1; %update winding number
+   end
+  
+end
+
+if ploton==1
+    %plot phase differences
+    figure(3); clf;
+    plot(phase_diff, 'o','MarkerSize',12);
+    xlabel('oscillator #');  ylabel('Phase Rel. to Oscillator #1');
+end
+
+%ouput
+p_diff = phase_diff(2);
+
+end
+
---
+++ case_comparison_2pt/make_init_cond_2pt_case.m
@@ -0,0 +1,43 @@
+function [ init_cond ] = make_init_cond_2pt_case( cycle, T_i, phase_diff)
+%make_init_cond Creates the initial condition vector
+%   for the pair of oscillators
+%   Input: cycle - timeseries of single-oscillator cycle
+%          T_i  - period (index-wise) of cycle
+%          phase_diff - phase-diff of the pair
+%   Output: Vector of initial conditions for each state variable
+
+
+%if no phase_diff passed, default to 0
+if nargin < 3
+  phase_diff = 0;
+end
+
+%turn 0= < phase_diff <= 1 into an indexed phase-diff
+ind_pd = round(T_i*phase_diff);
+
+%IC - phase_diff between the two modules
+K_0 = zeros(2,1);
+AV_0 = zeros(2,1);
+AD_0 = zeros(2,1);
+volt_V_0  = zeros(2,1);
+volt_D_0 = zeros(2,1);
+
+%phase of oscillator 1
+    K_0(1) = cycle(1,1);
+    AV_0(1) = cycle(1,4);
+    AD_0(1) = cycle(1,5);
+    volt_V_0(1) = cycle(1,2);
+    volt_D_0(1) = cycle(1,3);
+
+%phase of oscillator 2
+    K_0(2) = cycle(ind_pd,1);
+    AV_0(2) = cycle(ind_pd,4);
+    AD_0(2) = cycle(ind_pd,5);
+    volt_V_0(2) = cycle(ind_pd,2);
+    volt_D_0(2) = cycle(ind_pd,3);
+
+%initial condition
+init_cond = [K_0; AV_0; AD_0; volt_V_0; volt_D_0;];
+
+end
+
